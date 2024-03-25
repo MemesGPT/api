6e7e6c083215a8e9cad74aea16c9036590724786
Punches <punche@yandex.ru>
2024-03-25 21:07:59 +0300

    add first test

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,16 +1,40 @@
 import asyncio
 import typing
 
+import fastapi.testclient as fastapi_pytest_plugin
 import pytest
+import pytest_asyncio
+
+import lib.app.fastapi as app
+import lib.app.fastapi.settings as app_settings
+import tests.functional.utils.types as test_utils_types
+
+pytest_plugins = "tests.functional.fixtures"
 
 
-@pytest.fixture(scope="session", name="event_loop")
-def fixture_event_loop() -> typing.Generator[asyncio.AbstractEventLoop, typing.Any, None]:
-    loop = asyncio.new_event_loop()
-    yield loop
-    loop.close()
+@pytest.fixture(scope="session", name="loop")
+def fixture_loop(event_loop: asyncio.AbstractEventLoop) -> asyncio.AbstractEventLoop:
+    return event_loop
 
 
-__all__ = [
-    "fixture_event_loop",
-]
+@pytest.fixture(scope="session", name="settings")
+def fixture_settings() -> app_settings.Settings:
+    return app_settings.Settings()
+
+
+@pytest_asyncio.fixture(scope="session", name="application")
+async def fixture_application(
+    settings: app_settings.Settings,
+) -> typing.AsyncGenerator[app.Application, None]:
+    application = app.Application.from_settings(settings)
+
+    yield application
+
+    await application.dispose()
+
+
+@pytest.fixture(name="http_client")
+def fixture_http_client(
+    application: app.Application,
+) -> test_utils_types.TestClient:
+    return fastapi_pytest_plugin.TestClient(application._fastapi_app)  # noqa: SLF001
