339c9f8ef7d26550eb71954caf24dd356ef5d4f9
Punches <punche@yandex.ru>
2024-03-31 21:34:13 +0300

    fix alias for JokeScheme

--- backend/lib/api/rest/v1/joke/schemes/joke.py
+++ backend/lib/api/rest/v1/joke/schemes/joke.py
@@ -15,6 +15,9 @@ class JokeWithoutIdScheme(JokeCreateScheme):
 class JokeScheme(JokeWithoutIdScheme):
     joke_id: uuid.UUID = pydantic.Field(..., alias="id")
 
+    class Config:
+        allow_population_by_field_name = True
+
 
 __all__ = [
     "JokeScheme",
